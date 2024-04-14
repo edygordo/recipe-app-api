bfc422e44d16f3e956eb9649efc6ee63a7163340
edygordo <aditya.sahu.96@gmail.com>
2024-04-14 11:28:19 +0530

    Added a more robust email validation scheme-Linting Att-1

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,34 @@
-from django.db import models # noqa
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+import re
 
 # Create your models here.
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_field):
+        " A function to handle creating a user. "
+        email = self.normalize_email(email=email)
+        email_regex = "^[^@\s#$%*()&~`!\^]+@[^@\s#$%*()&~`!\^]+\.(com|net|org|gov|example)$"
+        valid = re.match(pattern=email_regex,string=email)
+        if valid is None:
+            raise ValueError('User must have a valid email address!')
+        user = self.model(email=email, **extra_field)
+        user.set_password(raw_password=password)
+        user.save(using=self._db)
+        return user
+    
+    # def create_superuser(self, email, name, password):
+    #     " A function to handle creating a superuser."
+    #     user = self.model(email=email, name=name)
+    #     user.is_superuser = True
+    #     return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    email = models.EmailField(max_length=255,unique=True)
+    name = models.CharField(max_length=255)
+
+    is_staff = models.BooleanField(default=False)
+    USERNAME_FIELD = 'email' # Use email field for login
+
+    objects = UserManager()
---
+++ app/core/tests/test_model.py
@@ -0,0 +1,31 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class ModelTests(TestCase):
+    "A class to handle tests over Authenticatio user model"
+
+    def test_create_user_with_email_successful(self):
+        "Create a user and test it's in db"
+        email = 'aditya@example.com'
+        password = "changeme"
+        user = get_user_model().objects.create_user(email=email, password=password)
+        self.assertEqual(user.email,email)
+        self.assertTrue(user.check_password(raw_password=password))
+
+    def test_create_user_with_normalized_email_successful(self):
+        "Create user who has a normalized email"
+        emails = ['aditya1@Example.CoM','aditya2@examplE.COM', 'AdityA3@examPle.com', 'aditya_sahU.96@gMaiL.cOm']
+        results= ['aditya1@example.com','aditya2@example.com', 'AdityA3@example.com', 'aditya_sahU.96@gmail.com']
+        for index,email in enumerate(emails):
+            user = get_user_model().objects.create_user(email=email)
+            self.assertEqual(user.email,results[index])
+
+    def test_invalid_email_throws_error(self):
+        "A user should always have a valid email"
+        
+        emails = ['', ' ', 'aditya1@exam', 'aditya@#$^gmail.com','ad@gmail@com', 'a`~d@gmail.com',
+                  'a%d@gmail.com']
+
+        for email in emails:
+            with self.assertRaises(ValueError):
+                user = get_user_model().objects.create_user(email=email)
